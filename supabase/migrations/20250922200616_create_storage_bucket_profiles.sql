7921ac3c02a4e69eb793fc01cc68dd7323a6f882
sohail tanveer <sohailtanveer769@gmail.com>
2025-09-22 15:08:07 -0500

    create supabase storage bucket profiles

---
+++ supabase/migrations/20250922200616_create_storage_bucket_profiles.sql
@@ -0,0 +1,7 @@
+
+insert into storage.buckets (id, name, public) values ('profiles', 'profiles', true);
+
+create policy "insert_profiles_bucket_authenticated"
+on storage.objects for insert to authenticated with check (
+    bucket_id = 'profiles'
+);
